572e993237e74e5a8007443d06a02cb1d538165d
throwbackjams <yli17@alumni.nd.edu>
2022-06-07 22:04:26 -0500

    test: small correction in TRACING

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -12,10 +12,10 @@ static TRACING: Lazy<()> = Lazy::new(|| {
     let subscriber_name = "test".to_string();
 
     if std::env::var("TEST_LOG").is_ok() {
-        let subscriber = get_subscriber("test".into(), "debug".into(), std::io::stout);
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
         init_subscriber(subscriber);
     } else {
-        let subscriber = get_subscriber("test".into(), "debug".into(), std::io::sink);
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
         init_subscriber(subscriber);
     };
 });
