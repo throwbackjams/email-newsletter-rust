9c2b32d047f5bfbcd7daeb5eaf41ba4e9b4dd123
throwbackjams <yli17@alumni.nd.edu>
2022-06-09 12:09:28 -0500

    subscriptions: add new-type pattern in domain.rs to validate names of subscribers in subscribe handler

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,8 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
+use crate::domain::{NewSubscriber, SubscriberName};
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
@@ -22,7 +24,13 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    match insert_subscriber(&connection_pool, &form).await {
+
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name)
+    };
+
+    match insert_subscriber(&connection_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
@@ -30,11 +38,11 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber in the database",
-    skip(form, connection_pool)
+    skip(new_subscriber, connection_pool)
 )]
 pub async fn insert_subscriber(
     connection_pool: &PgPool,
-    form: &FormData,
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -42,8 +50,8 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.inner_ref(),
         Utc::now()
     )
     .execute(connection_pool)
